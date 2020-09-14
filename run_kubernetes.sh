3b251749903d0bb74f04a0b07eeb2d63c4c20f66
Cristian Rodriguez <crisrod@Cristians-MacBook-Pro.local>
2020-09-14 15:23:28 -0300

    Files Last

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,23 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="crisroddev/udacityproject"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microservice\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=microservice
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods 
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward microservice-674574fb5c-m7zzh 8000:80
 
+# Step 5
+# Get LOGS 
+kubectl logs `kubectl get pods -o=name`
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+export dockerpath="crisroddev/udacityproject"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag udacityproject $dockerpath 
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath 
+ 
